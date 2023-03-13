128dd466ad6da4258410e85b0ddd2f2b9c4aae17
Gustavo Balero <gubalero@hotmail.com>
2023-03-13 01:21:18 -0300

    Ajustando a model Prova

--- src/main/java/br/com/fiap/cponline/models/Prova.java
+++ src/main/java/br/com/fiap/cponline/models/Prova.java
@@ -9,4 +9,64 @@ public class Prova {
     private String urlAcesso;
     private LocalDate data;
     private boolean ativo;
+
+    public Prova() {
+    
+    }
+
+    public Prova(int id, String descricao, String urlAcesso, LocalDate data, boolean ativo) {
+        this.id = id;
+        this.descricao = descricao;
+        this.urlAcesso = urlAcesso;
+        this.data = data;
+        this.ativo = ativo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getUrlAcesso() {
+        return urlAcesso;
+    }
+
+    public void setUrlAcesso(String urlAcesso) {
+        this.urlAcesso = urlAcesso;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    @Override
+    public String toString() {
+        return "Prova [id=" + id + ", descricao=" + descricao + ", urlAcesso=" + urlAcesso + ", data=" + data
+                + ", ativo=" + ativo + "]";
+    }
+
+    
 }
